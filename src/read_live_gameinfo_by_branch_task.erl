04fda228fd8cce30a2bd392f779738c8f8d396b3
Serhii Nechyporhuk <sergey.n@sbtech.com>
2016-08-26 17:49:13 +0300

    add time metric

--- src/read_live_gameinfo_by_branch_task.erl
+++ src/read_live_gameinfo_by_branch_task.erl
@@ -13,30 +13,36 @@
 -export([run/1]).
 
 -define(TASK_SLEEP, 50).
--define(RATE, atom_to_binary(?MODULE, utf8)).
+-define(TASK, <<(atom_to_binary(?MODULE, utf8))/binary, "_metrics">>).
+-define(RATE, <<?TASK/binary, ".rate">>).
+-define(TIME, <<?TASK/binary, ".time">>).
 
 create_indexes(Connection) ->
     mc_worker_api:ensure_index(Connection, <<"gameinfo">>, #{<<"key">> => {<<"BranchID">>, <<"hashed">>}}).
 
 job(Connection) ->
 
-    {_Status, _Res} = mc_worker_api:command(Connection, #{
-        <<"aggregate">> => <<"gameinfo">>,
-        <<"pipeline">> => [
-            #{
-                <<"$group">> => #{
-                    <<"_id">> => #{<<"BranchID">> => <<"$BranchID">>},
-                    <<"gameinfos">> => #{
-                        <<"$push">> => #{
-                            <<"gameinfo">> => <<"$$CURRENT">>
+    {Time, _Value} = timer:tc(
+        fun() ->
+            mc_worker_api:command(Connection, #{
+                <<"aggregate">> => <<"gameinfo">>,
+                <<"pipeline">> => [
+                    #{
+                        <<"$group">> => #{
+                            <<"_id">> => #{<<"BranchID">> => <<"$BranchID">>},
+                            <<"gameinfos">> => #{
+                                <<"$push">> => #{
+                                    <<"gameinfo">> => <<"$$CURRENT">>
+                                }
+                            }
                         }
                     }
-                }
-            }
-        ]
-    }),
+                ]
+            })
+        end),
 
     metrics:notify({?RATE, 1}),
+    metrics:notify({?TIME, Time}),
     timer:sleep(?TASK_SLEEP),
 
     job(Connection).
@@ -44,4 +50,5 @@ job(Connection) ->
 run(Connection) ->
     create_indexes(Connection),
     metrics:create(meter, ?RATE),
+    metrics:create(gauge, ?TIME),
     job(Connection).
