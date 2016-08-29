47f543dc9cb4d304541074d801a156948c687d96
dmitry.o <dmitry.o@sbtech.com>
2016-08-29 13:18:35 +0300

    add some metrics and optimize indexes

--- src/update_odd_task.erl
+++ src/update_odd_task.erl
@@ -4,20 +4,28 @@
 -export([run/1]).
 
 -define(TASK_SLEEP, 10).
--define(MARKET_INFO_METRIC, <<"update_market_info">>).
-
+-define(TASK, <<(atom_to_binary(?MODULE, utf8))/binary, "_metrics">>).
+-define(RATE, <<?TASK/binary, ".rate">>).
+-define(TIME, <<?TASK/binary, ".time">>).
 
 %%%===================================================================
 %%% API
 %%%===================================================================
 
+create_indexes(Connection) ->
+    mc_worker_api:ensure_index(Connection, <<"marketinfo">>, #{<<"key">> => {?ID, <<"hashed">>}}),
+    mc_worker_api:ensure_index(Connection, <<"marketinfo">>, #{<<"key">> => {<<"Selections.ID">>,1}}).
+
+
+
 run(Connection) ->
     
     %% init metrics
+    metrics:create(meter, ?RATE),
+    metrics:create(gauge, ?TIME),
 
-    metrics:create(meter, ?MARKET_INFO_METRIC),
-    
     %% Mian task
+    create_indexes(Connection),
     job(Connection).
 
 job(Connection) ->
@@ -26,9 +34,12 @@ job(Connection) ->
                 SelectionId = generator:rand_nth(SelectionIds),
                 Query = #{?ID => MarketId, <<"Selections.ID">> => SelectionId},
                 Command = #{<<"$set">> => #{ <<"Selections.$.Odds">> => generator:new_odd()}},
-%                error_logger:info_msg("Query ~p Command ~p", [Query, Command]),
-                mc_worker_api:update(Connection, <<"marketinfo">>, Query, Command),
-                metrics:notify({?MARKET_INFO_METRIC, 1});
+                {Time, _Value} = timer:tc(
+                                   fun() ->
+                                           mc_worker_api:update(Connection, <<"marketinfo">>, Query, Command)
+                                   end),
+                metrics:notify({?RATE, 1}),
+                metrics:notify({?TIME, Time});
             _ -> emtpy_meta_storage
         end,
 
