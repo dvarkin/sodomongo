4914c4309537619854aa441e50393529116cbe3f
Serhii Nechyporhuk <sergey.n@sbtech.com>
2016-08-30 10:49:49 +0300

    SB-67367: add checking commands

--- src/init_test.erl
+++ src/init_test.erl
@@ -5,27 +5,25 @@
 -export([run/1]).
 
 run(Connection) ->
-    mc_worker_api:command(Connection, #{<<"dropDatabase">> => 1}),
-    mc_worker_api:command(Connection, #{<<"enableSharding">> => ?DB}),
-
-    mc_worker_api:command(Connection, #{<<"drop">> => ?GAMEINFO}),
-    mc_worker_api:command(Connection, #{<<"drop">> => ?MARKETINFO}),
-
-    mc_worker_api:command(Connection, #{<<"create">> => ?GAMEINFO}),
-    mc_worker_api:command(Connection, #{<<"create">> => ?MARKETINFO}),
+    {true, _} = mc_worker_api:command(Connection, #{<<"dropDatabase">> => 1}),
+    {true, _} = mc_worker_api:command(Connection, #{<<"enableSharding">> => ?DB}),
+    {true, _} = mc_worker_api:command(Connection, #{<<"drop">> => ?GAMEINFO}),
+    {true, _} = mc_worker_api:command(Connection, #{<<"drop">> => ?MARKETINFO}),
+    {true, _} = mc_worker_api:command(Connection, #{<<"create">> => ?GAMEINFO}),
+    {true, _} = mc_worker_api:command(Connection, #{<<"create">> => ?MARKETINFO}),
 
     mc_worker_api:ensure_index(Connection, ?MARKETINFO, #{<<"key">> => {?ID, <<"hashed">>}}),
     mc_worker_api:ensure_index(Connection, ?MARKETINFO, #{<<"key">> => {<<"Selections.ID">>, 1}}),
     mc_worker_api:ensure_index(Connection, ?GAMEINFO, #{<<"key">> => {?GAME_ID, <<"hashed">> }}),
     mc_worker_api:ensure_index(Connection, ?GAMEINFO, #{<<"key">> => {?BRANCH_ID, <<"hashed">> }}),
 
-    mc_worker_api:command(Connection, #{
+    {true, _} = mc_worker_api:command(Connection, #{
         <<"shardCollection">> => ?GAMEINFO,
         <<"key">> => #{?GAME_ID => <<"hashed">>}
     }),
-    mc_worker_api:command(Connection, #{
-        <<"shardCollection">> => ?MARKETINFO,
-        <<"key">> => #{?ID => <<"hashed">>}
+    {true, _} = mc_worker_api:command(Connection, #{
+        <<"shardCollection">> => <<"marketinfo">>,
+        <<"key">> => #{<<"ID">> => <<"hashed">>}
     }).
 
 
