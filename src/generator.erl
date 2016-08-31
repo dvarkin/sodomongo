823862f2e1bf1022d0a5c911a73da14f3b0a0b78
Serhii Nechyporhuk <sergey.n@sbtech.com>
2016-08-31 17:07:32 +0300

    SB-67367: set branch deviation to 30

--- src/generator.erl
+++ src/generator.erl
@@ -21,7 +21,7 @@
 -define(MIN_GENERATION, 1).
 -define(MAX_GENERATION, 1000).
 -define(MIN_BRANCH, 1).
--define(MAX_BRANCH, 500).
+-define(MAX_BRANCH, 30).
 -define(MIN_LEAGUE_ID, 1).
 -define(MAX_LEAGUE_ID, 100000).
 -define(MIN_GROUP_ID, 1).
