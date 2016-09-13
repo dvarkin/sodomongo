632317845260a37992f80476adc67a062564c1dc
dmitry.o <dmitry.o@sbtech.com>
2016-09-13 11:51:18 +0300

    add init replica test

--- src/sodomongo.erl
+++ src/sodomongo.erl
@@ -1,6 +1,6 @@
 -module(sodomongo).
 
--export([start/0, start_deps/0, start_test/4, init_sharded_test/1]).
+-export([start/0, start_deps/0, start_test/4, init_sharded_test/1, init_replica_test/0]).
 
 -include("generator.hrl").
 
@@ -55,27 +55,55 @@ init_sharded_test(Db) ->
         <<"key">>, {?ID, <<"hashed">>}
     }).
 
+init_replica_test() ->
+    RawArgs = mongo:conn_args(),
+    
+    {ok, DbConnection} = mongo:connect_to_master(RawArgs),
+    AdminConnection = DbConnection,
+%    {ok, AdminConnection} = mongo:connect_to_master(AdminConnectionArgs),
+
+    {true, _} = mc_worker_api:command(DbConnection, #{<<"dropDatabase">> => 1}),
+    case mc_worker_api:command(AdminConnection, #{<<"drop">> => ?GAMEINFO}) of
+        {true, _} -> ok;
+        {false, #{<<"code">> := 26}} -> ok
+    end,
+    case mc_worker_api:command(AdminConnection, #{<<"drop">> => ?MARKETINFO}) of
+        {true, _} -> ok;
+        {false, #{<<"code">> := 26}} -> ok
+    end,
+    {true, _} = mc_worker_api:command(AdminConnection, #{<<"create">> => ?GAMEINFO}),
+    {true, _} = mc_worker_api:command(AdminConnection, #{<<"create">> => ?MARKETINFO}),
+
+    mc_worker_api:ensure_index(DbConnection, ?MARKETINFO, #{<<"key">> => {?ID, <<"hashed">>}}),
+    mc_worker_api:ensure_index(DbConnection, ?MARKETINFO, #{<<"key">> => {<<"Selections.ID">>, 1}}),
+    mc_worker_api:ensure_index(DbConnection, ?GAMEINFO, #{<<"key">> => {?ID, <<"hashed">> }}),
+    mc_worker_api:ensure_index(DbConnection, ?GAMEINFO, #{<<"key">> => {?BRANCH_ID, <<"hashed">> }}).
+
 init_metrics() ->
     insert_gameinfo_task:init_metrics(),
     insert_marketinfo_task:init_metrics(),
     update_odd_task:init_metrics(),
+    delete_gameinfo_task:init_metrics(),
+    delete_marketinfo_task:init_metrics(),
     read_branches_with_active_games_worker:init_metrics(),
     read_leagues_by_branch_with_number_of_games_in_active_state_worker:init_metrics(),
     read_live_gameinfo_by_branch_worker:init_metrics(),
     read_non_zero_odds_markets_worker:init_metrics(),
     read_top_events_by_turnover_worker:init_metrics(),
     read_top_events_starting_soon_worker:init_metrics(),
-
     ok.
 
 start_test(InsertWorkers, UpdateWorkers, ReadWorkers, Time) ->
+    init_replica_test(),
     init_metrics(),
     hugin:start_job(insert_gameinfo_task, InsertWorkers, Time, 1000),
     hugin:start_job(insert_marketinfo_task, InsertWorkers, Time, 10),
+    %hugin:start_job(delete_gameinfo_task, InsertWorkers, Time, 1200),
+    hugin:start_job(delete_marketinfo_task, InsertWorkers, Time, 20),
 
     hugin:start_job(update_odd_task, UpdateWorkers, Time, 10),
 
-    hugin:start_job(read_branches_with_active_games_worker, ReadWorkers, Time, 500),
+    %hugin:start_job(read_branches_with_active_games_worker, ReadWorkers, Time, 500),
     hugin:start_job(read_leagues_by_branch_with_number_of_games_in_active_state_worker, ReadWorkers, Time, 500),
     hugin:start_job(read_live_gameinfo_by_branch_worker, ReadWorkers, Time, 500),
     hugin:start_job(read_non_zero_odds_markets_worker, ReadWorkers, Time, 500),
