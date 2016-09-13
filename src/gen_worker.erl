ef6b71def287f66af1be772e95424baef60794cf
dmitry.o <dmitry.o@sbtech.com>
2016-09-13 23:34:32 +0300

    fix normal exit for workers

--- src/gen_worker.erl
+++ src/gen_worker.erl
@@ -53,8 +53,8 @@ start_link(Module, Args, Time, Sleep) ->
 
 init([Module, ConnectionArgs, Time, Sleep] = Args) ->
     self() ! connect,
-    timer:kill_after(Time),
-    %timer:apply_after(Time, gen_server, stop, [self()]),
+    %timer:kill_after(Time),
+    timer:apply_after(Time, gen_server, stop, [self()]),
     Metrics = make_metrics_titles(Module),
     Module_State = Module:init(Args),
     {ok, #state{connection_args = ConnectionArgs, 
