50a5f07cdf6d63420dad1911de1eded4817d3a80
Eugeny Linchenko <eugeny.l@sbtech.com>
2016-09-20 13:57:53 +0300

    add metrics report spawn

--- src/metrics.erl
+++ src/metrics.erl
@@ -39,7 +39,7 @@ create(Type, Name) ->
 
 -spec notify({binary() | atom(), any()}) -> ok.
 notify(Arg) ->
-    gen_server:call(?SERVER, {notify, Arg}),
+    gen_server:cast(?SERVER, {notify, Arg}),
     ok.
 
 %%%===================================================================
@@ -49,11 +49,12 @@ notify(Arg) ->
 init([]) ->
     {ok, #state{}}.
 
-handle_call({notify, Arg}, _From, State) ->
-    folsom_metrics:notify(Arg),
-    {reply, ok, State};
 handle_call(_Request, _From, State) ->
     {reply, ok, State}.
+
+handle_cast({notify, Arg}, State) ->
+    spawn(fun() -> folsom_metrics:notify(Arg) end),
+    {noreply, State};
 
 handle_cast({create, counter, Name}, State) ->
     folsom_metrics:new_counter(Name),
