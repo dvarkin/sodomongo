3589b69fed637c80beed1e55618a6e348de09535
Eugeny Linchenko <eugeny.l@sbtech.com>
2016-09-20 14:43:35 +0300

    fix example

--- src/worker_example.erl
+++ src/worker_example.erl
@@ -5,32 +5,35 @@
 -include("generator.hrl").
 
 %% API
-
 -export([init_metrics/0, job/2, init/1, start/3]).
-
-init_metrics() ->
-    gen_worker:init_metrics(?MODULE).
 
 %%%===================================================================
 %%% API
 %%%===================================================================
 
-start(ConnectionArgs, Time, SleepTimer) -> 
+init_metrics() ->
+    gen_worker:init_metrics(?MODULE).
+
+start(ConnectionArgs, Time, SleepTimer) ->
     gen_worker:start(?MODULE, ConnectionArgs, Time, SleepTimer).
 
 init(_Init_Args) ->
-    {ok, undefined_state}.
+    #{branches_ids => []}.
 
-job(Connection, State) ->
-    SomeInfo = 1,
-    {ok, some_query(Connection, SomeInfo), State}.
 
-%%%===================================================================
-%%% Internal functions
-%%%===================================================================
+job({_, SlaveConn}, State) ->
+    {ok, query(SlaveConn), State}.
 
-some_query({MasterConn, _SecCon}, _Info) ->
+query(Connection) ->
     fun() ->
-        mc_worker_api:find_one(MasterConn, <<"gameinfo">>, {<<"ID">>, 1}),
-        #{doc_count => 1, status => success}
+        Query = #{
+            ?ID => 1
+        },
+        Collection = <<"gameinfo">>,
+        Cursor = mc_worker_api:find(
+            Connection,
+            Collection,
+            Query
+        ),
+        util:parse_find_response(Cursor)
     end.
