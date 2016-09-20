5bb01f14797af7fc80a35608aa7cac15bf74e5b9
Serhii Nechyporhuk <sergey.n@sbtech.com>
2016-09-20 18:39:44 +0300

    set timeout to 1 ms

--- src/sodomongo.erl
+++ src/sodomongo.erl
@@ -105,18 +105,18 @@ start_test(InsertWorkers, UpdateWorkers, DeleteWorkers, ReadWorkers, Time) ->
     init_replica_test(),
     init_metrics(),
     meta_storage:flush(),
-    hugin:start_job(insert_gameinfo_task, InsertWorkers, Time, 100),
-    hugin:start_job(insert_marketinfo_task, InsertWorkers, Time, 100),
+    hugin:start_job(insert_gameinfo_task, InsertWorkers, Time, 1),
+    hugin:start_job(insert_marketinfo_task, InsertWorkers, Time, 1),
 
-    hugin:start_job(update_odd_task, UpdateWorkers, Time, 100),
+    hugin:start_job(update_odd_task, UpdateWorkers, Time, 1),
 
-    hugin:start_job(delete_gameinfo_task, DeleteWorkers, Time, 1000),
-    hugin:start_job(delete_marketinfo_task, DeleteWorkers, Time, 100),
+    hugin:start_job(delete_gameinfo_task, DeleteWorkers, Time, 1),
+    hugin:start_job(delete_marketinfo_task, DeleteWorkers, Time, 1),
 
-    hugin:start_job(read_branches_with_active_games_worker, ReadWorkers, Time, 500),
-    hugin:start_job(read_leagues_by_branch_with_number_of_games_in_active_state_worker, ReadWorkers, Time, 100),
-    hugin:start_job(read_live_gameinfo_by_branch_worker, ReadWorkers, Time, 100),
-    hugin:start_job(read_non_zero_odds_markets_worker, ReadWorkers, Time, 100),
+    hugin:start_job(read_branches_with_active_games_worker, ReadWorkers, Time, 1),
+    hugin:start_job(read_leagues_by_branch_with_number_of_games_in_active_state_worker, ReadWorkers, Time, 1),
+    hugin:start_job(read_live_gameinfo_by_branch_worker, ReadWorkers, Time, 1),
+    hugin:start_job(read_non_zero_odds_markets_worker, ReadWorkers, Time, 1),
     %% hugin:start_job(read_top_events_by_turnover_worker, ReadWorkers, Time, 100),
     %% hugin:start_job(read_top_events_starting_soon_worker, ReadWorkers, Time, 100),
 
